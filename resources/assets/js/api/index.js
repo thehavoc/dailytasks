413a851696af253c1d85838c34d38bfb9d8aeecf
AAtanasov <allen@htmlburger.com>
2017-11-19 13:34:08 +0200

    Add comments to the api class

--- resources/assets/js/api/index.js
+++ resources/assets/js/api/index.js
@@ -1,19 +1,25 @@
 /**
- * This is a class that will handle Ajax requests.
+ * This is a class that handles all Ajax requests of the app
  */
 
 import Route from '../route/index.js';
 
 export default class {
 
+	/**
+	 * Load the route class.
+	 */
 	constructor() {
 		this.route = new Route();
 	}
 
 	/**
 	 * Send a request to a given URL. 
-	 * A callback function is executed after the request.
-	 */
+	 * @param {String} url
+	 * @param {Object} data
+	 * @param {String} method
+	 * @return {Promise}
+	 */	
 	execute(url, data, method = 'get') {
 		if(!url) {
 			return false;
@@ -36,18 +42,42 @@ export default class {
 		});
 	}
 
+	/**
+	 * Execute a GET request
+	 * @param {String} url
+	 * @param {Object} data
+	 * @return {Promise}
+	 */
 	get(url, data) {
 		return this.execute(url, data, 'get');
 	}
 
+	/**
+	 * Execute a POST request
+	 * @param {String} url
+	 * @param {Object} data
+	 * @return {Promise}
+	 */
 	post(url, data) {
 		return this.execute(url, data, 'post');
 	}
 
+	/**
+	 * Execute a PATCH request
+	 * @param {String} url
+	 * @param {Object} data
+	 * @return {Promise}
+	 */
 	patch(url, data) {
 		return this.execute(url, data, 'patch');
 	}	
 
+	/**
+	 * Execute a DELETE request
+	 * @param {String} url
+	 * @param {Object} data
+	 * @return {Promise}
+	 */
 	delete(url ,data) {
 		return this.execute(url, data, 'delete');
 	}
